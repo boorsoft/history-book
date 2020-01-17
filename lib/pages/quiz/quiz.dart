5ec5cf25279b1a53cf43786443ab571be223ea3c
akmatoff <azim.akmatov1@gmail.com>
2020-01-17 22:42:51 +0600

    Designed the quiz screen

--- lib/pages/quiz/quiz.dart
+++ lib/pages/quiz/quiz.dart
@@ -28,6 +28,31 @@ class Quiz extends StatefulWidget {
   _QuizState createState() => _QuizState();
 }
 
+Widget OptionButton() {
+  return Padding(
+    padding: EdgeInsets.symmetric(
+      vertical: 15.0,
+      horizontal: 20.0,
+    ),
+    child: MaterialButton(
+      onPressed: () {},
+      child: Text(
+        "Вариант 1",
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'Blogger',
+          fontSize: 16.0,
+        ),
+      ),
+      color: Colors.green,
+      splashColor: Colors.lightGreen,
+      highlightColor: Colors.green[700],
+      minWidth: 150.0, 
+      height: 38.0,
+    ),
+  );
+}
+
 class _QuizState extends State<Quiz> {  
   @override 
   Widget build(BuildContext context) {
@@ -35,8 +60,49 @@ class _QuizState extends State<Quiz> {
       appBar: AppBar(title: Text('Тестирование')),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-                 
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: EdgeInsets.all(14.5),
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Здесь будет вопрос',
+                  style: TextStyle(
+                    fontSize: 16.5,
+                    fontFamily: 'Helvetica'
+                  ),
+                  ),
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    OptionButton(),
+                    OptionButton(),
+                    OptionButton(),
+                    OptionButton(),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Text(
+                  "30", 
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontFamily: 'Blogger',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
         ],
         )
       ),
