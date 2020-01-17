11cd862394327f65ae696acde02ae63825d83d0a
akmatoff <azim.akmatov1@gmail.com>
2020-01-17 22:12:48 +0600

    added json and statefulwidget for quiz screen

--- lib/pages/quiz/quiz.dart
+++ lib/pages/quiz/quiz.dart
@@ -1,4 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+class GetJson extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context).loadString("assets/quiz.json"),
+      builder: (context, snapshot) {
+        var data = json.decode(snapshot.data.toString());
+        if(data == null) {
+          return Scaffold(
+            body: Center(
+              child:
+              Text('Выполняется загрузка...', style: TextStyle(fontFamily: 'Blogger'))
+            ),
+          );
+        } else {
+          return Quiz();
+        }
+      }
+    );
+  }
+}
 
 class Quiz extends StatefulWidget {
   @override 
@@ -13,7 +36,7 @@ class _QuizState extends State<Quiz> {
       body: Container(
         child: Column(
           children: <Widget>[
-                  
+                 
         ],
         )
       ),
