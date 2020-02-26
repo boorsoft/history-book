c5a7a3837530029e8f98b4d70322fd51a1b76c4d
akmatoff <azim.akmatov1@gmail.com>
2020-02-26 22:22:40 +0600

    QuizButton for each json

--- lib/pages/quiz/quiz-starter.dart
+++ lib/pages/quiz/quiz-starter.dart
@@ -24,7 +24,13 @@ class _QuizStarterState extends State<QuizStarter> {
             QuizButton("Тест №2", "assets/json/exam2.json"),
             QuizButton("Тест №3", "assets/json/exam3.json"),
             QuizButton("Тест №4", "assets/json/exam4.json"),
-            QuizButton("Тест №5", "assets/json/exam5.json")
+            QuizButton("Тест №5", "assets/json/exam5.json"),
+            QuizButton("Тест №6", "assets/json/exam6.json"),
+            QuizButton("Тест №7", "assets/json/exam7.json"),
+            QuizButton("Тест №8", "assets/json/exam8.json"),
+            QuizButton("Тест №9", "assets/json/exam9.json"),
+            QuizButton("Тест №10", "assets/json/exam10.json"),
+            QuizButton("Тест №11", "assets/json/exam11.json"),
           ],
         )));
   }
